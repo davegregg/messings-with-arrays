e263530da3fed103655e75b2385b157db355afd2
davegregg@gmail.com <davegregg@gmail.com>
2021-08-24 10:45:41 -0400

    demo code start

---
+++ main.js
@@ -0,0 +1,19 @@
+"use strict";
+
+/*
+    A car object looks like:
+    {
+        "id": 1,
+        "make": "Volkswagen",
+        "model": "Jetta",
+        "modelYear": 2011,
+        "color": "Purple",
+        "numberOfDoors": 2,
+        "price": "$5325.89",
+        "vin": "1G4CW54K844908680"
+    }
+*/
+
+
+
+console.log(cars)
